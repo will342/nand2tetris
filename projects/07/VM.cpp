e0ea8c8df07014eab65bf6abca0d9d7e68c28239
Will Zochodne <w.zochodne@gmail.com>
2025-09-25 15:28:51 -0700

    assignment 7 completed

--- projects/07/VM.cpp
+++ projects/07/VM.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <unordered_map>
+#include <filesystem>
 
 class Parser {
 	public:
@@ -53,11 +54,15 @@ class Parser {
 	void advance();
 	std::string arg1();
 	const int arg2();
-	bool isPushPopFuncCall(commandTypes cmd);
+	bool isPushPopFuncCall();
+	bool isPushPop();
 };
 
 class Writer {
 	public:
+	std::ofstream outputFile;
+	int labelCounter = 1;
+
 	Writer(const std::string& inputFilename); 
 	void writeArithmetic(const std::string& command); 
 	void writePushPop(Parser::commandTypes command, const std::string& segment, int index); 
@@ -66,14 +71,301 @@ class Writer {
 
 Writer::Writer(const std::string& inputFilename) {
 	// opens the output file/stream and gets ready to write
+	std::filesystem::path inputPath(inputFilename);
+    std::filesystem::path outputPath = inputPath.replace_extension(".asm");
+    outputFile.open(outputPath);
+    if (!outputFile.is_open()) {
+    	std::cerr << "Error opening output file: " << outputPath << std::endl;
+        //Handle error as needed
+    }
+
 }
 
 void Writer::writeArithmetic(const std::string& command) {
 	// writes the assembly code that is the translation of the given arithmetic command
-}
+
+	if (command == "add") {
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "D=M\n" << "@SP\n" << "M=M-1\n"
+		 			<< "A=M\n" << "D=D+M\n" << "@SP\n" << "A=M\n" << "M=D\n" << "@SP\n" << "M=M+1\n";
+	}
+
+	if (command == "sub") {
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "D=M\n" << "@SP\n" << "M=M-1\n"
+		 			<< "A=M\n" << "D=M-D\n" << "@SP\n" << "A=M\n" << "M=D\n" << "@SP\n" << "M=M+1\n";
+	}
+
+	if (command == "neg") {
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "M=-M\n" << "@SP\n" << "M=M+1\n";
+	}
+
+	if (command == "eq") {
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "D=M\n" << "@SP\n" << "M=M-1\n"
+		 			<< "A=M\n" << "D=M-D\n" << "@SP\n" << "A=M\n" <<"M=0\n" << "@EQ_TRUE" << labelCounter << "\n" << "D;JEQ\n"
+					<<"@EQ_CONT" << labelCounter << "\n" << "0;JMP\n" << "(EQ_TRUE" << labelCounter << ")\n" << "@SP\n" << "A=M\n"
+					<< "M=-1\n" << "(EQ_CONT" << labelCounter << ")\n" << "@SP\n" << "M=M+1\n";
+		labelCounter++;
+	}
+
+	if (command == "gt") {
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "D=M\n" << "@SP\n" << "M=M-1\n"
+					<< "A=M\n" << "D=M-D\n" << "@SP\n" << "A=M\n" <<"M=0\n"<< "@GT_TRUE" << labelCounter << "\n" << "D;JGT\n"
+					<< "@GT_CONT" << labelCounter << "\n" << "0;JMP\n" << "(GT_TRUE" << labelCounter << ")\n" << "@SP\n" << "A=M\n"
+					<< "M=-1\n" << "(GT_CONT" << labelCounter << ")\n" << "@SP\n" << "M=M+1\n";
+		labelCounter++;
+	}
+
+	if (command == "lt") {
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "D=M\n" << "@SP\n" << "M=M-1\n"
+					<< "A=M\n" << "D=M-D\n"<< "@SP\n" << "A=M\n" <<"M=0\n" << "@LT_TRUE" << labelCounter << "\n" << "D;JLT\n"
+					<< "@LT_CONT" << labelCounter << "\n" << "0;JMP\n" << "(LT_TRUE" << labelCounter << ")\n" << "@SP\n" << "A=M\n"
+					<< "M=-1\n" << "(LT_CONT" << labelCounter << ")\n" << "@SP\n" << "M=M+1\n";
+		labelCounter++;
+	}
+
+	if (command == "and") {
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "D=M\n" << "@SP\n" << "M=M-1\n"<< "A=M\n" << "M=D&M\n" << "@SP\n" << "M=M+1\n";
+	}
+
+	if(command == "or") {
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "D=M\n" << "@SP\n" << "M=M-1\n"<< "A=M\n" << "M=D|M\n" << "@SP\n" << "M=M+1\n";
+	}
+
+	if(command == "not"){
+		outputFile << "@SP\n" << "M=M-1\n" << "A=M\n" << "M=!M\n" << "@SP\n" << "M=M+1\n";
+	}
+}	
 
 void Writer::writePushPop(Parser::commandTypes command, const std::string& segment, int index) {
 	// writes the assembly code that is the translation of the given push or pop command
+
+	if (command == Parser::C_PUSH) {
+		if (segment == "constant"){
+			outputFile << "@" << index << "\n" << "D=A\n" << "@SP\n" << "A=M\n" << "M=D\n" << "@SP\n" << "M=M+1\n";
+		}
+
+		if (segment == "local") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A" << "\n"
+			<< "@LCL" << "\n"
+			<< "A=M" << "\n"
+			<< "A=A+D" << "\n"
+			<< "D=M" << "\n"
+			<< "@SP" << "\n"
+			<< "A=M" << "\n"
+			<< "M=D" << "\n"
+			<< "@SP" << "\n"
+			<< "M=M+1\n";
+		}
+
+		if (segment == "argument") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A" << "\n"
+			<< "@ARG" << "\n"
+			<< "A=M" << "\n"
+			<< "A=A+D" << "\n"
+			<< "D=M" << "\n"
+			<< "@SP" << "\n"
+			<< "A=M" << "\n"
+			<< "M=D" << "\n"
+			<< "@SP" << "\n"
+			<< "M=M+1\n";
+		}
+		
+		if (segment == "this") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A" << "\n"
+			<< "@THIS" << "\n"
+			<< "A=M" << "\n"
+			<< "A=A+D" << "\n"
+			<< "D=M" << "\n"
+			<< "@SP" << "\n"
+			<< "A=M" << "\n"
+			<< "M=D" << "\n"
+			<< "@SP" << "\n"
+			<< "M=M+1\n";
+		}
+
+		if (segment == "that") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A" << "\n"
+			<< "@THAT" << "\n"
+			<< "A=M" << "\n"
+			<< "A=A+D" << "\n"
+			<< "D=M" << "\n"
+			<< "@SP" << "\n"
+			<< "A=M" << "\n"
+			<< "M=D" << "\n"
+			<< "@SP" << "\n"
+			<< "M=M+1\n";
+		}
+
+		if (segment == "temp") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A" << "\n"
+			<< "@5" << "\n"
+			<< "A=A+D" << "\n"
+			<< "D=M" << "\n"
+			<< "@SP" << "\n"
+			<< "A=M" << "\n"
+			<< "M=D" << "\n"
+			<< "@SP" << "\n"
+			<< "M=M+1\n";
+		}
+
+		if (segment == "pointer") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A" << "\n"
+			<< "@3" << "\n"
+			<< "A=A+D" << "\n"
+			<< "D=M" << "\n"
+			<< "@SP" << "\n"
+			<< "A=M" << "\n"
+			<< "M=D" << "\n"
+			<< "@SP" << "\n"
+			<< "M=M+1\n";
+		}
+
+		if (segment == "static") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A" << "\n"
+			<< "@16" << "\n"
+			<< "A=A+D" << "\n"
+			<< "D=M" << "\n"
+			<< "@SP" << "\n"
+			<< "A=M" << "\n"
+			<< "M=D" << "\n"
+			<< "@SP" << "\n"
+			<< "M=M+1\n";
+		}
+	}	
+
+	if (command == Parser::C_POP) {
+		if (segment == "local") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A\n"
+			<< "@LCL\n"
+			<< "D=D+M\n"
+			<< "@R13\n"
+			<< "M=D\n"
+			<< "@SP\n"
+			<< "M=M-1\n"
+			<< "A=M\n"
+			<< "D=M\n"
+			<< "@R13\n"
+			<< "A=M\n"
+			<< "M=D\n";
+			}
+			
+		if (segment == "argument") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A\n"
+			<< "@ARG\n"
+			<< "D=D+M\n"
+			<< "@R13\n"
+			<< "M=D\n"
+			<< "@SP\n"
+			<< "M=M-1\n"
+			<< "A=M\n"
+			<< "D=M\n"
+			<< "@R13\n"
+			<< "A=M\n"
+			<< "M=D\n";
+			}
+
+		if (segment == "this") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A\n"
+			<< "@THIS\n"
+			<< "D=D+M\n"
+			<< "@R13\n"
+			<< "M=D\n"
+			<< "@SP\n"
+			<< "M=M-1\n"
+			<< "A=M\n"
+			<< "D=M\n"
+			<< "@R13\n"
+			<< "A=M\n"
+			<< "M=D\n";
+			}
+
+		if (segment == "that") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A\n"
+			<< "@THAT\n"
+			<< "D=D+M\n"
+			<< "@R13\n"
+			<< "M=D\n"
+			<< "@SP\n"
+			<< "M=M-1\n"
+			<< "A=M\n"
+			<< "D=M\n"
+			<< "@R13\n"
+			<< "A=M\n"
+			<< "M=D\n";
+			}
+
+		if (segment == "temp") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A\n"
+			<< "@5\n"
+			<< "D=D+A\n"
+			<< "@R13\n"
+			<< "M=D\n"
+			<< "@SP\n"
+			<< "M=M-1\n"
+			<< "A=M\n"
+			<< "D=M\n"
+			<< "@R13\n"
+			<< "A=M\n"
+			<< "M=D\n";
+			}
+
+		if (segment == "pointer") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A\n"
+			<< "@3\n"
+			<< "D=D+A\n"
+			<< "@R13\n"
+			<< "M=D\n"
+			<< "@SP\n"
+			<< "M=M-1\n"
+			<< "A=M\n"
+			<< "D=M\n"
+			<< "@R13\n"
+			<< "A=M\n"
+			<< "M=D\n";
+			}
+
+		if (segment == "static") {
+			outputFile
+			<< "@" << index << "\n"
+			<< "D=A\n"
+			<< "@16\n"
+			<< "D=D+A\n"
+			<< "@R13\n"
+			<< "M=D\n"
+			<< "@SP\n"
+			<< "M=M-1\n"
+			<< "A=M\n"
+			<< "D=M\n"
+			<< "@R13\n"
+			<< "A=M\n"
+			<< "M=D\n";
+			}
+	}
 }
 
 Writer::~Writer() {
@@ -154,15 +446,22 @@ std::string Parser::arg1() {
 	return "";
 }
 
-bool Parser::isPushPopFuncCall(Parser::commandTypes cmd) {
+bool Parser::isPushPopFuncCall() {
+	Parser::commandTypes cmd = commandType();
     return cmd == Parser::C_PUSH
         || cmd == Parser::C_POP
         || cmd == Parser::C_FUNCTION
         || cmd == Parser::C_CALL;
 }
 
+bool Parser::isPushPop() {
+	Parser::commandTypes cmd = commandType();
+    return cmd == Parser::C_PUSH
+        || cmd == Parser::C_POP;
+}
+
 const int Parser::arg2() {
-	if (isPushPopFuncCall(commandType()) && commandTokens.size() > 2) {
+	if (isPushPopFuncCall() && commandTokens.size() > 2) {
 		return std::stoi(commandTokens[2]);
 	}
 	return -1;
@@ -170,33 +469,33 @@ const int Parser::arg2() {
 
 
 int main() {
-	const std::string& vmFile = "MemoryAccess/BasicTest/BasicTest.vm";
+	const std::string& vmFile = "MemoryAccess/StaticTest/StaticTest.vm";
 
 	std::cout << "Initializing parser with file " << vmFile << "\n\n";
 	Parser parser(vmFile);
 
+	Writer writer(vmFile);
+
 	while (parser.hasMoreCommands()) {
 		parser.advance();
+
+		//terminal output
 		std::cout << "Current command : " << parser.currentCommand << std::endl;
 		std::cout << "Command type : " << parser.commandType() << std::endl;
 		std::cout << "arg1 : " << parser.arg1() << std::endl;
-		if(parser.isPushPopFuncCall(parser.commandType())) {
+		if(parser.isPushPopFuncCall()) {
 			std::cout << "arg2 : " << parser.arg2() << std::endl;
 		}
+
+		//writer actions
+		if (parser.isPushPop()) {
+			writer.writePushPop(parser.commandType(), parser.arg1(), parser.arg2());
+		}
+
+		if (parser.commandType() == Parser::C_ARITHMETIC) {
+			writer.writeArithmetic(parser.currentCommand);
+		}
 	}
-
-	Writer writer(vmFile);
-
-	/*
-	stage 1 - implement the 9 stack arithmetic and logical commands as well as the push constant x command
-	(generic push command for the special case where the first argument is constant and the second argument is some decimal constant)
-
-	stage 2 - include a full implementation of push and pop commands handling all eight memory segments
-		2a - constant segment (already handled)
-		2b - handle the segments local, argument, this, and that
-		2c - pointer and temp segments - in particular allowing mod of the basses of this and that segments
-		2d - static segments
-	*/
-
+	
 	return 0;
 }
